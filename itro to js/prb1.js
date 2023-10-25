b8f04be9260d07cf7cf427f0a38410e734ad5ed2
sunnykdangi <144759982+sunnykdangi@users.noreply.github.com>
2023-10-25 21:14:36 +0530

    intro

---
+++ itro to js/prb1.js
@@ -0,0 +1,9 @@
+let MyName;
+MyName = "Sunny";
+console.log(MyName);
+MyName = "FatherName";
+FatherName="Neelesh";
+console.log(FatherName);
+FatherName ="MotherName";
+MotherName="Sanju";
+console.log(MotherName);
